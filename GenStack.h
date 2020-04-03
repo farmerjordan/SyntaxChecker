dce24a155c8579e6c9893b2ca5822a53d8377afd
Jordan Farmer <farmer@chapman.edu>
2020-04-03 05:13:06 +0000

    Genstack template header file complete.

---
+++ GenStack.h
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+template <class GS>
+
+class GenStack{
+
+public:
+  GenStack();//default constructor
+  GenStack(int maxSize); //overload constructor
+  ~GenStack(); //destructor
+
+  //Core functions
+  void push (GS x)
+  GS pop();
+
+  //Helper functions
+  GS peek();
+  bool isEmpty();
+  bool isFull();
+
+  int top;
+  int size;
+
+  GS *myArray;
+
+};
